345a914d28e73775f76e94ad75c30f46bc13cb62
xiezhuojin <innoveda@hotmail.com>
2021-12-28 20:19:40 +0800

    first django app part 4(before using generic view

--- django_site/polls/views.py
+++ django_site/polls/views.py
@@ -1,5 +1,6 @@
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -15,8 +16,18 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", { "question": question })
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", { "question": question })
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render("polls/detail.html", { "question": question, "error_message": "You didn't select a choice" })
+    else:
+        # should use f to let handle by database
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
